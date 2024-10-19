147ba072740b0f1d962cd58707daf720b52637be
rosto-infinity <rostoinfocus@gmail.com>
2024-10-19 09:49:07 +0100

    get3

--- classes/User.class.php
+++ classes/User.class.php
@@ -1,24 +1,67 @@
 <?php
-class User{
-  protected $name;
-  protected $password;
+// class User{
+//   protected $name;
+//   protected $password;
 
-  public function __construct($name, $password){
-    $this->name = $name;
-    $this->password = $password;
-  }
-  // public function getName(){
-  //   return $this->name;
-  // }
-  // public function getPassword(){
-  //   return $this->password;
-  // }
+//   public function __construct($name, $password){
+//     $this->name = $name;
+//     $this->password = $password;
+//   }
+//   // public function getName(){
+//   //   return $this->name;
+//   // }
+//   // public function getPassword(){
+//   //   return $this->password;
+//   // }
 
-  public function  FindUser()
-  { 
-    echo "Je suis "
-    .$this->name.
-    " et mon pass est : "
-    .$this->password." <br> ";
+//   public function  FindUser()
+//   { 
+//     echo "Je suis "
+//     .$this->name.
+//     " et mon pass est : "
+//     .$this->password." <br> ";
+//     }
+
+
+
+
+
+class User {
+    protected $name;
+    protected $password;
+
+    public function __construct($name, $password) {
+        $this->name = $name;
+        $this->setPassword($password);
     }
+
+    public function getName() {
+        return $this->name;
+    }
+
+    public function setName($name) {
+        $this->name = $name;
+    }
+
+    public function getPassword() {
+        return $this->password;
+    }
+
+    public function setPassword($password) {
+        // Hash the password before storing it
+        $this->password = password_hash($password, PASSWORD_DEFAULT);
+    }
+
+    public function verifyPassword($password) {
+        // Verify the password against the stored hash
+        return password_verify($password, $this->password);
+    }
+
+    public function findUser() {
+        echo "Je suis " . $this->name . " et mon pass est : " . $this->password . " <br> ";
+    }
+
+    public function isValidUser($name, $password) {
+      return $this->name === $name && $this->verifyPassword($password);
+  }
 }
--- index.php
+++ index.php
@@ -2,16 +2,16 @@
 require "classes/User.class.php";
 require "classes/Admin.class.php";
 
-$user1 = new User("Eva", "0000");
-$user2 = new User("Lele", "4200");
-$user1->FindUser();
-$user2->FindUser();
-echo "###################3  Admin ########################### <br>";
-$admin1 = new Admin("Waffo", "0000",true);
-$admin2 = new Admin("lele", "0000",false);
-$admin3 = new Admin("lele", "0000",false);
-$admin1->FindAdmin();
-$admin2->FindAdmin();
+// $user1 = new User("Eva", "0000");
+// $user2 = new User("Lele", "4200");
+// $user1->FindUser();
+// $user2->FindUser();
+// echo "###################3  Admin ########################### <br>";
+// $admin1 = new Admin("Waffo", "0000",true);
+// $admin2 = new Admin("lele", "0000",false);
+// $admin3 = new Admin("lele", "0000",false);
+// $admin1->FindAdmin();
+// $admin2->FindAdmin();
 
 
 //echo "###################3  avec get ########################### <br>";
@@ -27,4 +27,18 @@ $admin2->FindAdmin();
 // echo "#############  Admin ############## <br>";
 // echo  $admin->getName() . "<br>"; 
 // echo  $admin->getPassword().  "<br>";; 
-// echo  $admin->getAdmin(); 
+// echo  $admin->getAdmin(); // Exemple d'utilisation
+
+
+$user = new User('JohnDoe', 'password123');
+echo $user->getName(); // JohnDoe
+$user->setName('JaneDoe');
+echo $user->getName(); // JaneDoe
+
+if ($user->verifyPassword('password123')) {
+    echo "Mot de passe correct";
+} else {
+    echo "Mot de passe incorrect";
+}
+
+$user->findUser();
